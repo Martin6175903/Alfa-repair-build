5573c7847b5ce2d246373968c2dfe5b67319723e
Martin6175903 <martin6175903@yandex.ru>
2022-07-31 10:22:24 +0300

    added slider and modification

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+
+   $(".slider__items").slick({
+      slidesToShow: 1,
+      infinite: false,
+      prevArrow: '<button type="button" class="slick-prev"><img class="slick-prev__img" src="images/page2/slider-arrow-left.svg" alt="Prev"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img class="slick-next__img" src="images/page2/slider-arrow-right.svg" alt="Next"></button>'
+   });
+
