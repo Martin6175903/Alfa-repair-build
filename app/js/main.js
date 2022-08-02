9d0c08ac91cf6c56529e5e170ee56471f611b4d0
Martin6175903 <martin6175903@yandex.ru>
2022-08-02 07:04:32 +0300

    slider added styles and JS

--- app/js/main.js
+++ app/js/main.js
@@ -6,3 +6,14 @@
       nextArrow: '<button type="button" class="slick-next"><img class="slick-next__img" src="images/page2/slider-arrow-right.svg" alt="Next"></button>'
    });
 
+
+   window.onload = function () {
+      let sliderItemActiveBg = document.querySelector('.cases__house:first-of-type');
+      sliderItemActiveBg.classList.add(".cases__house--active");
+      let objStyles = {
+         background: '#319F1C',
+         color: '#fff',
+         fill: "white"
+      }
+      
+};
